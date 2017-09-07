97914d1d313ce53b4b31e818a18d8e68ceaf5a4f
suorness <inqire@gmail.com>
2017-09-08 00:25:42 +0300

    test eclipse

---
+++ BT/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("1Hello World!");
+        System.out.println("2Hello World!");
+        System.out.println("2Hello World!");
+        System.out.println("3Hello World!");
+    }
+}

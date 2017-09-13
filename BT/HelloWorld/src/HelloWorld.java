0d222073b98e855a4b7481b47be35d3d1a740454
suorness <inqire@gmail.com>
2017-09-14 02:07:27 +0300

    del

--- BT/HelloWorld/src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("1Hello World!");
-        System.out.println("2Hello World!");
-        System.out.println("2Hello World!");
-        System.out.println("3Hello World!");
-    }
-}

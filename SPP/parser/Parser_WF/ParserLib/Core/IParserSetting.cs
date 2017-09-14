878c124f02cd1e4f0f0bd63d9425c28fa5434918
suorness <inqire@gmail.com>
2017-09-14 03:36:36 +0300

    HttpLoader is ready

---
+++ SPP/parser/Parser_WF/ParserLib/Core/IParserSetting.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParserLib.Core
+{
+    interface IParserSetting
+    {
+        string BaseUrl { get; set; }
+    }
+}
